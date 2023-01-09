4ef348717ae403ff18daa6e2b01fa37bca60222c
jaawadalishah5 <livespotless@gmail.com>
2023-01-09 14:11:12 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebJob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+            config.Queues.MaxDequeueCount = 1;
+            config.Queues.BatchSize = 1;  //It is used to make sure that only 1 message is processed by the job at one time.                                         
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
